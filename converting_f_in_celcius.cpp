1d21c8da89f8cd1b9c6f9fcb1e4c23669566e81b
Mukul13242 <97087832+Mukul13242@users.noreply.github.com>
2022-01-04 13:43:35 +0530

    Add files via upload

---
+++ converting_f_in_celcius.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+//     int f;
+//     cout<<"Enter the value of feranhit:";
+//     cin>> f;
+//     int c=(f-32)*(5.0/9);
+//     cout<<"The value of c is :"<<c<<endl;
+double a =6/4;
+int b=6/4;
+double c=a+b;
+cout<<c;
+    
+    return 0;
+    }
