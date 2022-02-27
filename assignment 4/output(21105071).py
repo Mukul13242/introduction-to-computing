119eef6b13528a3d8351b3ad6ab11b6bd864ab75
Mukul13242 <97087832+Mukul13242@users.noreply.github.com>
2022-02-27 19:45:23 +0530

    Add files via upload

---
+++ assignment 4/output(21105071).py
@@ -0,0 +1,96 @@
+Python 3.9.10 (v3.9.10:f2f3f53782, Jan 13 2022, 16:55:45) 
+[Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: /Users/mukulgoyal/Desktop/codingninja/practice.py ==========
+Question 1
+Enter number of disks in tower of Hanoi:3
+The Disks are numbered starting from top of the tower. 
+      Steps to move all disks from Source Tower to Destination Tower is given below
+Move disk 1 fromSource TowertoDestination
+Move disk 2 from Source Tower Auxiliary
+Move disk 1 fromDestinationtoAuxiliary
+Move disk 3 from Source Tower Destination
+Move disk 1 fromAuxiliarytoSource Tower
+Move disk 2 from Auxiliary Destination
+Move disk 1 fromSource TowertoDestination
+>>> 
+========== RESTART: /Users/mukulgoyal/Desktop/codingninja/practice.py ==========
+Question 2
+Enter the number of rows in Pascal's Triangle: 8
+
+By Using recursions:
+
+              1   
+            1   1   
+          1   2   1   
+        1   3   3   1   
+      1   4   6   4   1   
+    1   5   10   10   5   1   
+  1   6   15   20   15   6   1   
+1   7   21   35   35   21   7   1   
+
+ Alternate method
+
+
+ By Using loops:
+
+              1   
+            1   1   
+          1   2   1   
+        1   3   3   1   
+      1   4   6   4   1   
+    1   5   10   10   5   1   
+  1   6   15   20   15   6   1   
+1   7   21   35   35   21   7   1   
+>>> 
+========== RESTART: /Users/mukulgoyal/Desktop/codingninja/practice.py ==========
+Question3
+
+Enter an Integer:134
+Enter another Integer:34
+
+The quotient when 134 is divided by 34 is 3.
+The remainder when 134 is divided by 34 is 32.
+
+Que3.a
+Function is Not-callable
+
+Que3.b
+All the values are non zero
+
+Que3.c
+Values greater than 4 is 32,5,6
+
+Que3.d
+Above result in set form is shown below:
+{32, 5, 6}
+
+Que3.e
+The above set has been converted to immutable.
+
+Que3.f
+Max value from set is 32
+Hash value of 32 is 32
+>>> 
+========== RESTART: /Users/mukulgoyal/Desktop/codingninja/practice.py ==========
+
+[Question.4]
+
+Hy, My name is mukul goyal and my roll no. is 21105071
+Destructor Called
+>>> 
+========== RESTART: /Users/mukulgoyal/Desktop/codingninja/practice.py ==========
+QUE 5
+a. The Mehak salary is updated to 70000
+b. Employee Viren record deleted
+>>> 
+========== RESTART: /Users/mukulgoyal/Desktop/codingninja/practice.py ==========
+QUESTION 6
+
+
+Enter Word by George:arc
+Enter Word by Barbie:car
+
+Friendship of Barbie and George is REAL
+>>> 
